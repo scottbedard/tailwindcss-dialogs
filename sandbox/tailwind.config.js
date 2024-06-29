318e705f5ab5e177a494598f008347922dab4b5b
Scott Bedard <scottbedard90@gmail.com>
2024-06-29 18:42:40 -0500

    cleanup

--- sandbox/tailwind.config.js
+++ sandbox/tailwind.config.js
@@ -7,7 +7,11 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: 'Quicksand, ui-sans-serif, system-ui, -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, "Noto Sans", sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji"',
+      },
+    },
   },
   plugins: [
     dialogs,
